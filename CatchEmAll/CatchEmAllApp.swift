fba48a6cea76cec3eba4e63b37191728a912fd11
Gerd Faedtke <gerd.faedtke@icloud.com>
2024-01-25 19:35:19 +0100

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Gerd Faedtke on 25.01.24.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
